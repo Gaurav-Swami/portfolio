aac2da018a4ab636b5fabe38dcd2850a61b3d27c
Gaurav-Swami <gauravswami0404@gmail.com>
2024-09-26 20:47:25 +0530

    scroll position on navbar items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 const Header: React.FC = () => {
-  const Button = ({ text, link }: { text: string; link: string }) => {
+  //$0.getBoundingClientRect().top + window.scrollY
+
+  const Button = ({ text, goToY }: { text: string; goToY: number }) => {
     return (
-      <button className=" text-2xl text-primaryText bg-accent py-4 w-56 rounded-full font-bold sticky hover:text-accent hover:bg-primaryText border-2 transition-all border-accent">
+      <button
+        onClick={() => window.scrollTo({ top: goToY })}
+        className=" text-2xl text-primaryText bg-accent py-4 w-56 rounded-full font-bold sticky hover:text-accent hover:bg-primaryText border-2 transition-all border-accent"
+      >
         <span className="text-center  ">{text}</span>
-        <span className="invisible absolute">{link}</span>
+
         {/* <div className=" inline ">
           <PiGreaterThanBold className="inline  " />
         </div> */}
@@ -13,10 +20,10 @@ const Header: React.FC = () => {
   return (
     <div className="flex  items-center justify-center w-full gap-x-4 mt-12 absolute top-[450px]">
       {/* <div className=""> */}
-        <Button text={"HOME"} link={"xxx"} />
-        <Button text={"ABOUT"} link={"xxx"} />
-        <Button text={"PROJECTS"} link={"xxx"} />
-        <Button text={"CONTACT"} link={"xxx"} />
+      <Button text={"HOME"} goToY={0} />
+      <Button text={"ABOUT"} goToY={740} />
+      <Button text={"PROJECTS"} goToY={1150} />
+      <Button text={"CONTACT"} goToY={3415} />
       {/* </div> */}
     </div>
   );

56dc28dee92e46cf9b5d33a66f4347e2a20b4d08
Gaurav-Swami <gauravswami0404@gmail.com>
2024-09-29 15:59:19 +0530

    desingned about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,65 @@
+import { DiVim } from "react-icons/di";
+
 const About = () => {
+  const skills = [
+    "CSS",
+    "HTML",
+    "JavaScript",
+    "React",
+    "MongoDB",
+    "NodeJS",
+    "ExpressJS",
+    "SQL",
+    "C/C++",
+    "Java",
+    "Python",
+    "Github",
+  ];
   return (
-    <div className="grid place-items-center  text-background py-[120px] px-16 bg-primaryText mt-2">
-      <span className="font-bold text-[40px] tracking-widest">ABOUT ME</span>
-      <span className="rounded-full bg-accent w-8 mt-4  h-[5px]"></span>
-      <span className="text-xl text-center pt-6">
-        Discover more about my background, what I specialize in, and the
-        programming and tech skills <br /> I&apos;ve developed throughout my
-        journey.
-      </span>
+    <div className="grid place-items-center py-[120px] text-background bg-primaryText  px-16">
+      <div className="grid place-items-center w-full   mt-2">
+        <span className="font-bold text-[40px] tracking-widest">ABOUT ME</span>
+        <span className="rounded-full bg-accent w-8 mt-4  h-[5px]"></span>
+        <span className="text-xl text-center pt-6">
+          Discover more about my background, what I specialize in, and the
+          programming and tech skills <br /> I&apos;ve developed throughout my
+          journey.
+        </span>
+      </div>
+      <div className="flex mt-[90px] gap-x-16">
+        <div className="w-[570px] text-lg">
+          <h2 className="text-[26px] font-bold pb-[30px]">
+            Know more about me!{" "}
+          </h2>
+          As a MERN stack developer, you excel at building dynamic, full-stack
+          web applications using MongoDB, Express.js, React, and Node.js. Your
+          proficiency in JavaScript enables you to create seamless user
+          interfaces and robust back-end systems, ensuring a smooth user
+          experience. <br />
+          <br />
+          You thrive on transforming ideas into functional applications,
+          leveraging your skills in RESTful API design and database management.
+          With a strong focus on responsive design and performance optimization,
+          you are passionate about writing clean, maintainable code. You enjoy
+          collaborating with cross-functional teams, embracing agile
+          methodologies, and staying updated with the latest technologies to
+          continuously enhance your projects and drive innovation.
+        </div>
+        <div>
+          <h2 className="text-[26px] font-bold pb-[30px]">My Skills   </h2>
+
+          <div className="flex w-[570px] gap-3 flex-wrap">
+            {skills.map((skill, index) => (
+              <button
+                className=" py-[10px] rounded-lg px-5 bg-[#DCDCDC] font-semibold opacity-85"
+                key={index}
+              >
+                {skill}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,9 +9,16 @@ import {
   FaYoutube,
 } from "react-icons/fa";
 
-// import { PiGreaterThanBold } from "react-icons/pi";
+// import { PiGreaterThanBold } from "react-icons/pi";shadow-lg
 
 const Links = () => {
+  const links = {
+    twitter: "https://x.com/itsgorv",
+    github: "https://www.github.com/gaurav-swami",
+    instagram: "https://www.instagram.com/gauravswaami/?hl=en",
+    linkedIn: "https://www.linkedin.com/in/gaurav-swami-b2005627a/",
+    youtube: "",
+  };
   const [visibleIcons, setVisibleIcons] = useState(0);
   const icons = 5;
   useEffect(() => {
@@ -19,43 +26,51 @@ const Links = () => {
       if (visibleIcons < icons) {
         setVisibleIcons((prev) => prev + 1);
       }
-    },250);
+    }, 250);
 
     return () => clearInterval(interval);
-  }, [visibleIcons,icons]);
+  }, [visibleIcons, icons]);
 
   return (
     <div className=" flex flex-col absolute left-0 top-1/2 transform -translate-y-1/2 text-3xl gap-y-7 gap-x-4 p-4">
-      <FaGithub
-        className={`hover:scale-110  duration-200 ease-out cursor-pointer relative ${
-          visibleIcons > 0 ? "left-0" : "-left-14"
-        }  `}
-      />
-      <FaLinkedin
-        className={`hover:scale-110  duration-200 ease-out cursor-pointer relative ${
-          visibleIcons > 1 ? "left-0" : "-left-14"
-        }  `}
-      />
-      <FaTwitter
-        className={`hover:scale-110  duration-200 ease-out cursor-pointer relative ${
-          visibleIcons > 2 ? "left-0" : "-left-14"
-        } `}
-      />
-      <FaInstagram
-        className={`hover:scale-110  duration-200 ease-out cursor-pointer relative ${
-          visibleIcons > 3 ? "left-0" : "-left-14"
-        } `}
-      />
-      <FaYoutube
-        className={`hover:scale-110  duration-200 ease-out cursor-pointer relative ${
-          visibleIcons > 4 ? "left-0" : "-left-14"
-        } `}
-      />
+      <a href={links.github} target="_blank">
+        <FaGithub
+          className={`hover:scale-110  duration-200 ease-out cursor-pointer relative ${
+            visibleIcons > 0 ? "left-0" : "-left-14"
+          }  `}
+        />
+      </a>
+      <a href={links.linkedIn} target="_blank">
+        <FaLinkedin
+          className={`hover:scale-110  duration-200 ease-out cursor-pointer relative ${
+            visibleIcons > 1 ? "left-0" : "-left-14"
+          }  `}
+        />
+      </a>
+      <a href={links.twitter} target="_blank">
+        <FaTwitter
+          className={`hover:scale-110  duration-200 ease-out cursor-pointer relative ${
+            visibleIcons > 2 ? "left-0" : "-left-14"
+          } `}
+        />
+      </a>
+      <a href={links.instagram} target="_blank">
+        <FaInstagram
+          className={`hover:scale-110  duration-200 ease-out cursor-pointer relative ${
+            visibleIcons > 3 ? "left-0" : "-left-14"
+          } `}
+        />
+      </a>
+      <a href="">
+        <FaYoutube
+          className={`hover:scale-110  duration-200 ease-out cursor-pointer relative ${
+            visibleIcons > 4 ? "left-0" : "-left-14"
+          } `}
+        />
+      </a>
     </div>
   );
 };
-
-
 
 export default function Hero() {
   return (
@@ -70,7 +85,6 @@ export default function Hero() {
           websites that effectively meet the needs of businesses.
         </div>
         <Links />
-        
       </div>
     </div>
   );
